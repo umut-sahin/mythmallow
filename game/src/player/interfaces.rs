abb67b3a8393852883888798f4e9fd34004fc2b3
Umut <umutsahin@protonmail.com>
2023-11-27 19:23:45 +0300

    docs: document interfaces

--- game/src/player/interfaces.rs
+++ game/src/player/interfaces.rs
@@ -3,14 +3,20 @@ use crate::prelude::*;
 
 /// Interface for mythologies.
 pub trait Mythology: Any + Debug + Send + Sync + 'static {
+    /// Gets the unique identifier of the mythology.
     fn id(&self) -> SmolStr;
+    /// Gets the name of the mythology.
     fn name(&self) -> SmolStr;
 }
 
 
 /// Interface for players.
 pub trait Playable: Debug + Send + Sync + 'static {
+    /// Gets the unique identifier of the player.
     fn id(&self) -> SmolStr;
+    /// Gets the name of the player.
     fn name(&self) -> SmolStr;
+
+    /// Spawns the player.
     fn spawn(&self, world: &mut World);
 }
