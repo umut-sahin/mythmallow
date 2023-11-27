c8e58e2aa383a39a4caacf5cb8422ce548b3a7ea
Umut <umutsahin@protonmail.com>
2023-11-27 19:23:45 +0300

    docs: document interfaces

--- game/src/items/interfaces.rs
+++ game/src/items/interfaces.rs
@@ -3,9 +3,16 @@ use crate::prelude::*;
 
 /// Interface for items.
 pub trait Item: Debug + Send + Sync + 'static {
+    /// Gets the unique identifier of the item.
     fn id(&self) -> SmolStr;
+    /// Gets the name of the item.
     fn name(&self) -> SmolStr;
+
+    /// Instantiates the item to add it to the inventory.
     fn instantiate(&self) -> ItemInstance;
+
+    // Acquires the item.
     fn acquire(&self, world: &mut World) -> Option<Entity>;
+    // Releases the item.
     fn release(&self, world: &mut World, entity: Option<Entity>);
 }
--- game/src/mode/interfaces.rs
+++ game/src/mode/interfaces.rs
@@ -3,8 +3,13 @@ use crate::prelude::*;
 
 /// Interface for game modes.
 pub trait Mode: Debug + Send + Sync + 'static {
+    /// Gets the unique identifier of the game mode.
     fn id(&self) -> SmolStr;
+    /// Gets the name of the game mode.
     fn name(&self) -> SmolStr;
+
+    /// Initializes the game mode.
     fn initialize(&self, world: &mut World);
+    /// Deinitializes the game mode.
     fn deinitialize(&self, world: &mut World);
 }
