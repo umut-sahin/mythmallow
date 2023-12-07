c57489641dde456f8fb9ad3badfa2b33ff906e76
Umut <umutsahin@protonmail.com>
2023-12-07 20:21:52 +0300

    feat: handle no players registered and only one player registered specially

--- game/src/ui/player_selection_screen/systems.rs
+++ game/src/ui/player_selection_screen/systems.rs
@@ -14,6 +14,21 @@ pub fn spawn_player_selection_screen(
     player_selection_screen_action_input_map: Res<InputMap<PlayerSelectionScreenAction>>,
     player_registry: Res<PlayerRegistry>,
 ) {
+    if player_registry.is_empty() {
+        // TODO: Replace panic with a proper error communicated through the UI.
+        panic!("no players are available");
+    }
+
+    if player_registry.number_of_players() == 1 {
+        let mythology_index = SelectedMythologyIndex(0);
+        let player_index = SelectedPlayerIndex(0);
+
+        commands.insert_resource(mythology_index);
+        commands.insert_resource(player_index);
+
+        return;
+    }
+
     let button_style = styles::button();
     let button_colors = WidgetColors::button();
     let button_font = asset_server.load("fonts/FiraSans-Bold.ttf");
