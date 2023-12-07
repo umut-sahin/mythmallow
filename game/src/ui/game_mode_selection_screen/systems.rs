90434fe9dc38fa1b49d5163ffdb73480fc30b391
Umut <umutsahin@protonmail.com>
2023-12-07 20:57:16 +0300

    feat: support multiple game modes

--- game/src/ui/game_mode_selection_screen/systems.rs
+++ game/src/ui/game_mode_selection_screen/systems.rs
@@ -1,9 +1,17 @@
-use crate::prelude::*;
+use crate::{
+    prelude::*,
+    ui::game_mode_selection_screen::{
+        constants::*,
+        styles,
+    },
+};
 
 
 /// Spawns the game mode selection screen.
 pub fn spawn_game_mode_selection_screen(
     mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    game_mode_selection_screen_action_input_map: Res<InputMap<GameModeSelectionScreenAction>>,
     game_mode_registry: Res<GameModeRegistry>,
 ) {
     if game_mode_registry.is_empty() {
@@ -11,14 +19,107 @@ pub fn spawn_game_mode_selection_screen(
         panic!("no game modes are available");
     }
 
-    if game_mode_registry.len() == 1 {
+    if game_mode_registry.number_of_game_modes() == 1 {
         let selected_game_mode_index = SelectedGameModeIndex(0);
         commands.insert_resource(selected_game_mode_index);
         return;
     }
 
-    // TODO: Add support for multiple game modes and a nice game mode selection screen.
-    panic!("multiple game modes are not supported at the moment")
+    let button_style = styles::button();
+    let button_colors = WidgetColors::button();
+    let button_font = asset_server.load("fonts/FiraSans-Bold.ttf");
+    let button_size = BUTTON_FONT_SIZE;
+
+    let mut entities = Vec::new();
+    let mut first = true;
+
+    for (game_mode_index, game_mode) in game_mode_registry.iter().enumerate() {
+        let game_mode_index = SelectedGameModeIndex(game_mode_index);
+        let game_mode_button = if first {
+            first = false;
+            Widget::button(
+                &mut commands,
+                (
+                    Name::new(format!("{} Button", game_mode.name())),
+                    GameModeSelectionScreenGameModeButton { game_mode_index },
+                    Widget::default().selected(),
+                    WidgetSelected::now(),
+                ),
+                &button_style,
+                button_colors,
+                &button_font,
+                button_size,
+                game_mode.name(),
+            )
+        } else {
+            Widget::button(
+                &mut commands,
+                (
+                    Name::new(format!("{} Button", game_mode.name())),
+                    GameModeSelectionScreenGameModeButton { game_mode_index },
+                    Widget::default(),
+                ),
+                &button_style,
+                button_colors,
+                &button_font,
+                button_size,
+                game_mode.name(),
+            )
+        };
+        entities.push(game_mode_button)
+    }
+
+    let back_button = Widget::button(
+        &mut commands,
+        (Name::new("Back Button"), GameModeSelectionScreenBackButton, Widget::default()),
+        &button_style,
+        button_colors,
+        &button_font,
+        button_size,
+        "Back",
+    );
+    entities.push(back_button);
+
+    for i in 0..entities.len() {
+        let up = if i != 0 { entities[i - 1] } else { entities[entities.len() - 1] };
+        let current = entities[i];
+        let down = if i != entities.len() - 1 { entities[i + 1] } else { entities[0] };
+
+        commands.entity(current).insert((WidgetUp(up), WidgetDown(down)));
+    }
+
+    let pressed = ActionData { state: ButtonState::Pressed, ..default() };
+    let mut action_state = ActionState::default();
+
+    action_state.set_action_data(GameModeSelectionScreenAction::Back, pressed.clone());
+    action_state.set_action_data(GameModeSelectionScreenAction::Up, pressed.clone());
+    action_state.set_action_data(GameModeSelectionScreenAction::Down, pressed.clone());
+    action_state.set_action_data(GameModeSelectionScreenAction::Select, pressed);
+
+    let mut game_mode_selection_screen = commands.spawn((
+        Name::new("GameMode Selection Screen"),
+        GameModeSelectionScreen,
+        InputManagerBundle::<GameModeSelectionScreenAction> {
+            action_state,
+            input_map: game_mode_selection_screen_action_input_map.clone(),
+        },
+        NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                flex_direction: FlexDirection::Column,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                row_gap: Val::Px(ROW_GAP),
+                ..default()
+            },
+            ..default()
+        },
+    ));
+
+    for entity in entities {
+        game_mode_selection_screen.add_child(entity);
+    }
 }
 
 /// Despawns the game mode selection screen.
@@ -28,6 +129,84 @@ pub fn despawn_game_mode_selection_screen(
 ) {
     if let Ok(entity) = game_mode_selection_screen_query.get_single() {
         commands.entity(entity).despawn_recursive();
+    }
+}
+
+
+/// Navigates the game mode selection screen using game mode selection screen actions.
+pub fn navigation(
+    mut commands: Commands,
+    mut game_mode_selection_screen_query: Query<
+        &ActionState<GameModeSelectionScreenAction>,
+        With<GameModeSelectionScreen>,
+    >,
+    mut selected_widget_query: Query<(&mut Widget, &WidgetUp, &WidgetDown), With<WidgetSelected>>,
+    mut next_app_state: ResMut<NextState<AppState>>,
+) {
+    let game_mode_selection_screen_action_state =
+        match game_mode_selection_screen_query.get_single_mut() {
+            Ok(query_result) => query_result,
+            Err(_) => return,
+        };
+    let (mut selected_widget, up_widget, down_widget) = match selected_widget_query.get_single_mut()
+    {
+        Ok(query_result) => query_result,
+        Err(_) => return,
+    };
+
+    if game_mode_selection_screen_action_state.just_pressed(&GameModeSelectionScreenAction::Back) {
+        next_app_state.set(AppState::MainMenu);
+        return;
+    }
+
+    if game_mode_selection_screen_action_state.just_pressed(&GameModeSelectionScreenAction::Select)
+    {
+        selected_widget.clicked = true;
+        return;
+    }
+
+    let go_up =
+        game_mode_selection_screen_action_state.just_pressed(&GameModeSelectionScreenAction::Up);
+    let go_down =
+        game_mode_selection_screen_action_state.just_pressed(&GameModeSelectionScreenAction::Down);
+
+    if (go_up || go_down) && !(go_up && go_down) {
+        if go_down {
+            commands.entity(down_widget.0).insert(WidgetSelected::now());
+        } else {
+            commands.entity(up_widget.0).insert(WidgetSelected::now());
+        }
+    }
+}
+
+
+/// Returns to the main menu.
+pub fn back_button_interaction(
+    mut back_button_query: Query<
+        &mut Widget,
+        (Changed<Widget>, With<GameModeSelectionScreenBackButton>),
+    >,
+    mut next_app_state: ResMut<NextState<AppState>>,
+) {
+    if let Ok(mut button) = back_button_query.get_single_mut() {
+        button.on_click(|| {
+            next_app_state.set(AppState::MainMenu);
+        });
+    }
+}
+
+/// Selects the game mode.
+pub fn game_mode_button_interaction(
+    mut commands: Commands,
+    mut game_mode_button_query: Query<
+        (&mut Widget, &GameModeSelectionScreenGameModeButton),
+        Changed<Widget>,
+    >,
+) {
+    for (mut button, metadata) in &mut game_mode_button_query {
+        button.on_click(|| {
+            commands.insert_resource(metadata.game_mode_index);
+        });
     }
 }
 
