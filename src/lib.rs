d378f1771c15b6f63d0ad48d7dfc2b7aa4096ab2
Umut <umutsahin@protonmail.com>
2024-01-02 15:23:59 +0300

    style: fix ordering of content modules in the top level library

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 #![doc = include_str!("../README.md")]
 pub use mythmallow_game::*;
 pub mod content {
-    pub mod modes {
-        pub mod survival {
-            pub use mythmallow_mode_survival::*;
-        }
-    }
     pub mod items {
         pub mod greek {
             pub use mythmallow_items_greek::*;
+        }
+    }
+    pub mod modes {
+        pub mod survival {
+            pub use mythmallow_mode_survival::*;
         }
     }
     pub mod players {
