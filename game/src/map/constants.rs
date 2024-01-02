af13f96aaa60f27de4349b4fb0d3439e468022f1
Umut <umutsahin@protonmail.com>
2024-01-02 21:35:48 +0300

    feat: spawn map bounds automatically

---
+++ game/src/map/constants.rs
@@ -0,0 +1,2 @@
+/// Thickness of map bounds.
+pub const BOUND_THICKNESS: f32 = 50.00;
--- game/src/map/systems.rs
+++ game/src/map/systems.rs
@@ -1,4 +1,62 @@
-use crate::prelude::*;
+use crate::{
+    map::constants::*,
+    prelude::*,
+};
+
+
+/// Spawns the bounds of the map.
+pub fn spawn_map_bounds(
+    mut commands: Commands,
+    map_bounds: Res<MapBounds>,
+    map_query: Query<Entity, With<Map>>,
+) {
+    let mut map = match map_query.get_single() {
+        Ok(map_entity) => commands.entity(map_entity),
+        Err(_) => return,
+    };
+    map.with_children(|parent| {
+        let layers = CollisionLayers::new(
+            [Layer::MapBound],
+            [Layer::Player, Layer::Enemy, Layer::Projectile],
+        );
+
+        let x_length = (map_bounds.x_max - map_bounds.x_min) + (4.00 * BOUND_THICKNESS);
+        let y_length = map_bounds.y_max - map_bounds.y_min;
+
+        parent.spawn((
+            Name::new("Left Bound"),
+            MapBound,
+            RigidBody::Static,
+            Collider::cuboid(2.00 * BOUND_THICKNESS, y_length),
+            layers,
+            Position(Vector::X * (map_bounds.x_min - BOUND_THICKNESS)),
+        ));
+        parent.spawn((
+            Name::new("Top Bound"),
+            MapBound,
+            RigidBody::Static,
+            Collider::cuboid(x_length, 2.00 * BOUND_THICKNESS),
+            layers,
+            Position(Vector::Y * (map_bounds.y_max + BOUND_THICKNESS)),
+        ));
+        parent.spawn((
+            Name::new("Right Bound"),
+            MapBound,
+            RigidBody::Static,
+            Collider::cuboid(2.00 * BOUND_THICKNESS, y_length),
+            layers,
+            Position(Vector::X * (map_bounds.x_max + BOUND_THICKNESS)),
+        ));
+        parent.spawn((
+            Name::new("Bottom Bound"),
+            MapBound,
+            RigidBody::Static,
+            Collider::cuboid(x_length, 2.00 * BOUND_THICKNESS),
+            layers,
+            Position(Vector::Y * (map_bounds.y_min - BOUND_THICKNESS)),
+        ));
+    });
+}
 
 
 /// Despawns the map.
