00d7b573c3fe8fa234b4068a6c28754eb6e21993
Umut <umutsahin@protonmail.com>
2024-04-29 15:50:41 +0300

    tweak: attraction speed of experience points

--- game/src/leveling/constants.rs
+++ game/src/leveling/constants.rs
@@ -9,10 +9,10 @@ pub const DEFAULT_EXPERIENCE_POINT_COLOR: Color = Color::PURPLE;
 
 
 /// Default minimum speed of experience points.
-pub const DEFAULT_EXPERIENCE_POINT_MIN_SPEED: Speed = Speed(300.00);
+pub const DEFAULT_EXPERIENCE_POINT_MIN_SPEED: Speed = Speed(-200.00);
 
 /// Default acceleration of experience points.
-pub const DEFAULT_EXPERIENCE_POINT_ACCELERATION_PER_SECOND: Speed = Speed(100.00);
+pub const DEFAULT_EXPERIENCE_POINT_ACCELERATION_PER_SECOND: Speed = Speed(1600.00);
 
 /// Default maximum speed of experience points.
-pub const DEFAULT_EXPERIENCE_POINT_MAX_SPEED: Speed = Speed(500.00);
+pub const DEFAULT_EXPERIENCE_POINT_MAX_SPEED: Speed = Speed(900.00);
