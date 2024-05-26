63e99a4d7fe46ac355449f2cb7b285496cdb5705
Umut <umutsahin@protonmail.com>
2024-05-26 18:09:32 +0300

    refactor: export game prelude from sweet enemies prelude and don't use game prelude

--- enemies/sweet/src/chocolate_bar.rs
+++ enemies/sweet/src/chocolate_bar.rs
@@ -1,9 +1,6 @@
 use {
     crate::prelude::*,
-    mythmallow::{
-        enemy::constants::RANGED_ENEMY_TAG,
-        prelude::*,
-    },
+    mythmallow::enemy::constants::RANGED_ENEMY_TAG,
 };
 
 /// Size of the enemy.
--- enemies/sweet/src/prelude.rs
+++ enemies/sweet/src/prelude.rs
@@ -4,3 +4,5 @@ pub use crate::{
     pack::SweetEnemyPack,
     plugin::SweetEnemiesPlugin,
 };
+
+pub(crate) use mythmallow::prelude::*;
